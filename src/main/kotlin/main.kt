6eafff2ebb478c992df5f8ed1015ad9d8d47b89a
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-17 18:23:19 -0300

    funcao

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
+fun calculaBonus(a: Int, b: Int, c: Int){
+
+    println("o bonus é de: ${a+b*c}")
+
+
+}
 fun main() {
     /*println("olá mundo!")
 
@@ -9,7 +15,7 @@ fun main() {
     val imutavel = 100; //val é uma variavel imutavel, equivalente a const em javascript
     val x: Int; // ou seja, pode ser atribuina se eu declarar o tipo explicidamente, ao contrario de uma const em javascript.
 
-     */
+
 
     val frase = "Kotlin é uma linguagem"
   val caracteristica = "show!"
@@ -24,4 +30,13 @@ val testeDeQuebraDeLinh = """sadasaosjoasdjoajdpasjda
     |asjdjaskdjaskdjakjdk"
 """.trimMargin()
     println(testeDeQuebraDeLinh.length)
+
+     */
+
+    val a =10;
+    val b =20;
+    val c = 30;
+
+    calculaBonus(a,b,c)
+
 }
