e85d8d130cded17f840dc6b731aa69aae77b8d74
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-17 18:11:17 -0300

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("olá mundo!")
+    /*println("olá mundo!")
 
     var nome ="victor"
     var tempo = 1.43232
@@ -8,5 +8,8 @@ fun main() {
 
     val imutavel = 100; //val é uma variavel imutavel, equivalente a const em javascript
     val x: Int; // ou seja, pode ser atribuina se eu declarar o tipo explicidamente, ao contrario de uma const em javascript.
+*/
 
+    val frase = "Kotlin é uma linguagem"
+    println(frase);
 }
